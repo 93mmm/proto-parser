5d816d9d72d4389ed4e6ef61e98f8901905834bc
93mmm <nickolaev513@gmail.com>
2025-05-31 02:32:51 +0300

    helpers moved

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -15,35 +15,3 @@ func NewProtoParser(src source.Source) *ProtoParser {
 	}
 }
 
-func (p *ProtoParser) extractWord() string {
-	word := make([]rune, 0, 30)
-
-	for !p.EOF() {
-		if p.PeekSymbol() {
-			word = append(word, p.Next())
-		} else {
-			break
-		}
-	}
-	return string(word)
-}
-
-func (p *ProtoParser) skipWhiteSpaces() {
-	for !p.EOF() {
-		if p.PeekWhiteSpace() {
-			p.Next()
-		} else {
-			break
-		}
-	}
-}
-
-func (p *ProtoParser) skipUntilNextLine() {
-	for !p.EOF() {
-		if !p.Peek('\n') {
-			p.Next()
-		} else {
-			break
-		}
-	}
-}
